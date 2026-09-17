f5fb764d46001cf2ea23fb96daa1816466aa8a60
Dana <45907654+jonesydev@users.noreply.github.com>
2026-09-16 22:38:51 -0400

    Delete database/scripts/seed-data/seed-data-artists.sql
    Don't need this here. New version will have seed data for all tables and be stored in "tests" directory.

--- database/scripts/seed-data/seed-data-artists.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO public.artists(
-	artist_name, styles, influences, artist_albums)
-	VALUES 
-	  ('Black Sabbath', ARRAY['Heavy Metal', 'Rock'], ARRAY['Cream', 'The Beatles', 'Fleetwood Mac'], ARRAY[1, 2]),
-      ('Metallica', ARRAY['Heavy Metal', 'Rock'], ARRAY['Diamond Head', 'Iron Maiden', 'Black Sabbath'], ARRAY[1, 2]),
-	  ('Judas Priest', ARRAY['Heavy Metal', 'Rock'], ARRAY['Led Zeppelin', 'Deep Purple', 'Black Sabbath'], ARRAY[1, 2]);
